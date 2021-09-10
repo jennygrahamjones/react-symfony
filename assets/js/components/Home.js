6ddf6b010da9d1f333bae802460d6f8f20647465
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2021-09-09 20:43:42 -0400

    just pokemon things

---
+++ assets/js/components/Home.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  text-align: center;
+  font-size: 10px;
+  font-smooth: never;
+  -webkit-font-smoothing: none;
+  font-family: "pokemon-font", monospace;
+`;
+
+const Pokemon = styled.div`
+  filter: grayscale(100%);
+`;
+
+const Home = () => {
+  const getPokemon = () => {
+    let random = Math.floor(Math.random() * 151) + 1;
+
+    axios.get(`http://localhost:8000/api/pokemon/${random}`).then((p) => {
+      console.log(p.data);
+      setPokemon(p.data);
+    });
+  };
+
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+  const move = pokemon?.moves.at(
+    Math.floor(Math.random() * pokemon.moves.length)
+  );
+
+  return (
+    <Container>
+      {pokemon && (
+        <Pokemon>
+          <img src={pokemon.sprites?.front_default} />
+          <p>
+            A wild {pokemon.name?.toUpperCase()} appeared! It used{" "}
+            {move.move.name.replace("-", " ").toUpperCase()}!
+          </p>
+        </Pokemon>
+      )}
+    </Container>
+  );
+};
+
+export default Home;
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,12 +9,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/{reactRouting}", name="home", defaults={"reactRouting": null})
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/api/pokemon/{id}", name="pokemon")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function getPokemon(int $id)
+    { 
+        $pokemon = file_get_contents('https://pokeapi.co/api/v2/pokemon/'.$id);
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->setContent($pokemon);
+        return $response;  
     }
 }
